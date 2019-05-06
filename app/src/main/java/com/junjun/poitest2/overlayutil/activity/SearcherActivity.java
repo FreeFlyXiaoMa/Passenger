6f0c5ea1774fde338c1fd958ce1f065e8e9fa698
FreeFlyXiaoMa <2429825530@qq.com>
2019-05-06 09:39:20 +0800

    做了一些修改

--- app/src/main/java/com/junjun/poitest2/overlayutil/activity/SearcherActivity.java
+++ app/src/main/java/com/junjun/poitest2/overlayutil/activity/SearcherActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -54,7 +55,7 @@ public class SearcherActivity extends AppCompatActivity implements TextWatcher, 
     private EditText etSearcher;
     private ImageView ivCancel;
     private ListView lvResult;
-
+    private LinearLayout search_history;
     private void initView(){
         etSearcher = (EditText) findViewById(R.id.et_searcher);
         if (isStartPort){
@@ -67,6 +68,8 @@ public class SearcherActivity extends AppCompatActivity implements TextWatcher, 
         ivCancel.setOnClickListener(this);
         lvResult = (ListView) findViewById(R.id.lvResult);
         lvResult.setOnItemClickListener(this);
+
+        search_history=findViewById(R.id.keyword_search_history);
     }
 
     //监听etSearcher文本变化的接口
@@ -80,6 +83,7 @@ public class SearcherActivity extends AppCompatActivity implements TextWatcher, 
         if (s == null){
             return;
         }
+
         PoiCitySearchOption option = new PoiCitySearchOption()
                 .city(city)
                 .keyword(s.toString())
@@ -101,18 +105,54 @@ public class SearcherActivity extends AppCompatActivity implements TextWatcher, 
         }
         ArrayList<HashMap<String, Object>> arrayList = new ArrayList<>();
         //关键字搜索
-        /*for (int i = 0; i < list.size(); i++){
+        for (int i = 0; i < list.size(); i++){
             HashMap<String,Object> map = new HashMap<>();
             map.put("name", list.get(i).name);
             map.put("address", list.get(i).address);
             map.put("location", list.get(i).location);
             arrayList.add(map);
-        }*/
-        for(int i=0;i<10;i++){
-
-
-
         }
+        /*search_history.setVisibility(View.GONE);
+        HashMap<String,Object> map1 = new HashMap<>();
+        map1.put("name", "北方工业大学");
+        map1.put("address","北京市石景山区晋元庄路5号");
+        arrayList.add(map1);
+        HashMap<String,Object> map2 = new HashMap<>();
+        map2.put("name", "北京邮电大学");
+        map2.put("address","北京市海淀区北土城路5号");
+        arrayList.add(map2);
+        HashMap<String,Object> map3 = new HashMap<>();
+        map3.put("name", "北京大学");
+        map3.put("address","北京市海淀区成府路205号");
+        arrayList.add(map3);
+        HashMap<String,Object> map4 = new HashMap<>();
+        map4.put("name", "北京市");
+        map4.put("address","北京市");
+        arrayList.add(map4);
+        HashMap<String,Object> map5 = new HashMap<>();
+        map5.put("name", "北京市国际饭店");
+        map5.put("address","北京市东城区建国门大街甲9号");
+        arrayList.add(map5);
+        HashMap<String,Object> map6 = new HashMap<>();
+        map6.put("name", "北京市植物园");
+        map6.put("address","北京市海淀区香山南路");
+        arrayList.add(map6);
+        HashMap<String,Object> map7 = new HashMap<>();
+        map7.put("name", "北京市旧机动车交易市场");
+        map7.put("address","北京市丰台区南四环西路123号");
+        arrayList.add(map7);
+        HashMap<String,Object> map8 = new HashMap<>();
+        map8.put("name", "北京市西郊宾馆");
+        map8.put("address","北京市海淀区王庄路18号");
+        arrayList.add(map8);
+        HashMap<String,Object> map9 = new HashMap<>();
+        map9.put("name", "北京锡华商务酒店");
+        map9.put("address","北京市海淀区颐和园路12号");
+        arrayList.add(map9);
+        HashMap<String,Object> map10 = new HashMap<>();
+        map10.put("name", "北京市朝阳公园");
+        map10.put("address","北京市朝阳区朝阳公园南路1号");
+        arrayList.add(map10);*/
 
         SimpleAdapter adapter = new SimpleAdapter(SearcherActivity.this, arrayList, R.layout.list_item,
                 new String[]{"name", "address"}, new int[]{R.id.tv_result_name, R.id.tv_result_address});
